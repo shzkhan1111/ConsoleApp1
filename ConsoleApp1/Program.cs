49af262d9ae4e589943812617604d759353aaff8
shzkhan1111 <shahzaib.ahmedkhan111@gmail.com>
2024-04-12 18:51:19 +0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            char[][] grid = new char[][] {
+            new char[] { '1', '1', '1', '1', '0' },
+            new char[] { '1', '1', '0', '1', '0' },
+            new char[] { '1', '1', '0', '0', '0' },
+            new char[] { '0', '0', '0', '0', '0' }
+        };
+
+        }
+    }
+}
